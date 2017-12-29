b77af20e02c0d55d7a2f1e34bb3f238852ecbe6c
matlap <matey@konto.pl>
2017-12-29 13:26:42 +0100

    error handler hoc fixes

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -24,22 +24,21 @@ class BurgerBuilder extends Component {
     //     this.state = {...}
     // }
     state = {
-        ingredients: {
-            salad: 0,
-            bacon: 0,
-            cheese: 0,
-            meat: 0
-        },
+        ingredients: null,
         totalPrice: 4,
         purchasable: false,
         purchasing: false,
-        loading: false
+        loading: false,
+        error: false
     }
 
     componentDidMount() {
         axios.get("/ingredients.json")
         .then(response => {
             this.setState({ingredients: response.data});
+        })
+        .catch(error => {
+            this.setState({error: true});
         });
     }
 
@@ -111,7 +110,7 @@ class BurgerBuilder extends Component {
             deliveryMethod: "fastest"
         };
 
-        axios.post("/orders", order)
+        axios.post("/orders.json", order)
             .then(response => {
                 this.setState({loading: false, purchasing: false});
             })
@@ -127,19 +126,9 @@ class BurgerBuilder extends Component {
         for ( let key in disabledInfo ) {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
-        
-        let orderSummary = <OrderSummary 
-            ingredients={this.state.ingredients}
-            price={this.state.totalPrice}
-            purchaseCancelled={this.purchaseCancelHandler}
-            purchaseContinued={this.purchaseContinueHandler}
-        />;
 
-        if (this.state.loading) {
-            orderSummary = <Spinner />
-        }
-
-        let burger = <Spinner />;
+        let orderSummary = null;
+        let burger = this.state.error ? <p>Ingredients can't be loaded.</p> : <Spinner />;
 
         if (this.state.ingredients) {
             burger = (
@@ -154,6 +143,17 @@ class BurgerBuilder extends Component {
                         price={this.state.totalPrice} />
                 </Aux>
             );
+
+            orderSummary = <OrderSummary 
+                ingredients={this.state.ingredients}
+                price={this.state.totalPrice}
+                purchaseCancelled={this.purchaseCancelHandler}
+                purchaseContinued={this.purchaseContinueHandler}
+            />;
+        }
+
+        if (this.state.loading) {
+            orderSummary = <Spinner />
         }
 
         return (
